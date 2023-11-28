62cfb38eea5bd7f90f813e14ff53507b8a4f9c4f
rooppi <prostoxer@icloud.com>
2023-11-28 15:30:51 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.WriteLine("Готов начать?");
+
+string qeshion = Console.ReadLine();
+
+int counter = 0;
+
+if (qeshion == "да")
+{
+    Console.WriteLine(" Реши пример √9=?");
+
+    int answer = int.Parse(Console.ReadLine());
+
+    if (answer == 3)
+    {
+        Console.WriteLine("Молодец");
+
+    }
+    if (answer != 3)
+    {
+
+        while (counter < 5)
+        {
+            Console.WriteLine("Ты лох, ответ не правельный)");
+
+            counter++;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("ну ладно, жаль");
+};
+
+Console.ReadKey();
